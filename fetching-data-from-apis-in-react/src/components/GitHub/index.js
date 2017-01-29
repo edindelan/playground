d84eaadf2e71e77177d31fb71f135ea20c546156
Edin Delan <edindelan@Edins-MacBook-Pro.local>
2017-01-29 16:06:33 +0100

    Fetching data

--- fetching-data-from-apis-in-react/src/components/GitHub/index.js
+++ fetching-data-from-apis-in-react/src/components/GitHub/index.js
@@ -2,22 +2,35 @@ import React, {Component} from 'react';
 
 class GitHub extends Component {
     state = {
+        requestFailed: false
+    };
 
-    }
-    componentDidMount(){
+    callApi = () => {
         fetch('https://api.github.com/users/edindelan')
+            .then(response => {
+                console.log(response);
+                if (!response.ok) {
+                    throw Error('Network request failed');
+                }
+                return response;
+            })
             .then(d => d.json())
             .then(r => {
                 this.setState({data: r})
+            }, () => {
+                this.setState({requestFailed: true})
             })
-    }
+    };
+
+    componentDidMount() {
+        this.callApi();
+    };
+
     render() {
-        console.log(this);
-        return (
-            <div>
-                GitHub
-            </div>
-        )
+        const {data, requestFailed} = this.state;
+        if (requestFailed) return <div>Failed...</div>;
+        if (!data) return <div>Loading...</div>;
+        if (data) return <div>{data.name}</div>;
     }
 }
 
